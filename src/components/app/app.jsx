07cf33baf08c8393e982b96fe0d6b357ca407c5c
Dmitry Lyapin <lyapindm@yandex.ru>
2022-05-04 11:57:14 +0300

    Удалил Context

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,59 +1,27 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useEffect } from 'react';
 import app from './app.module.css';
 
 import { AppHeader } from '../app-header/app-header';
 import { MainPage } from '../../pages/main-page/main-page';
-import { BurgerContext } from '../../services/burger-context';
 import { useDispatch } from 'react-redux';
 import { fetchIngredients } from '../../services/redux/slices/burger-ingredients';
-
-const initialState = {
-  ingredients: null,
-  bun: null,
-  toppings: [],
-  total: 0,
-  order: null,
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'SET_INGREDIENTS':
-      return { ...state, ingredients: action.payload };
-    case 'SET_BUN':
-      return { ...state, bun: action.payload };
-    case 'SET_TOPPINGS':
-      return { ...state, toppings: action.payload };
-    case 'SET_TOTAL':
-      return { ...state, total: action.payload };
-    case 'SET_ORDER':
-      return { ...state, order: action.payload };
-    default:
-      throw new Error(`Wrong type of action: ${action.type}`);
-  }
-};
 
 export const App = () => {
   const dispatch = useDispatch();
 
-  const [
-    burgerState,
-    burgerDispatcher,
-  ] = useReducer(reducer, initialState, undefined);
-
-  useEffect(() => {
-    dispatch(fetchIngredients());
-  }, [dispatch]);
+  useEffect(
+    () => {
+      dispatch(fetchIngredients());
+    },
+    [
+      dispatch,
+    ],
+  );
 
   return (
     <div className={app.app}>
-      <BurgerContext.Provider
-        value={{
-          burgerState,
-          burgerDispatcher,
-        }}>
-        <AppHeader />
-        <MainPage />
-      </BurgerContext.Provider>
+      <AppHeader />
+      <MainPage />
     </div>
   );
 };
