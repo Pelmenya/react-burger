c13581a8c014ee5cf1a3f7d4a3282a58449a1cbc
Dmitry Lyapin <lyapindm@yandex.ru>
2022-07-14 14:21:40 +0300

    Сделал e2e тесты для проверки оформления заказа

--- cypress/e2e/burger-constructor/burger-constructor.cy.js
+++ cypress/e2e/burger-constructor/burger-constructor.cy.js
@@ -4,6 +4,8 @@ describe('service is available', function (){
     // browser with a 720p monitor
     cy.viewport(1280, 720);
     cy.visit('http://localhost:3000');
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(3000);
   });
   it('should open burger constructor page by default and test modal', function (){
     cy.contains('Краторная булка N-200i').click();
@@ -49,6 +51,12 @@ describe('service is available', function (){
       .trigger('drop')
       .trigger('dragend');
     cy.get('button').contains('Оформить заказ').click();
-    cy.get('input[type*="email"]')
+
+    cy.get('input[type*="email"]').click().type('mock@email.com');
+    cy.get('input[type*="password"]').click().type('111111');
+    cy.get('button').contains('Войти').click();
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(2000);
+    cy.get('button').contains('Оформить заказ').click();
   });
 });
